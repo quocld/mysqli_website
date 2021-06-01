29977ca776156ae0a269c6630ac603973b88da7a
quocle2208 <80745853+quocle2208@users.noreply.github.com>
2021-06-01 15:11:42 +0700

    Update config.php

--- web_mysqli/admincp/config/config.php
+++ web_mysqli/admincp/config/config.php
@@ -1,6 +1,8 @@
 <?php
     
-    $mysqli = new mysqli("localhost","root","","web_mysqli");
+    $mysqli = new mysqli("ec2-54-91-188-254.compute-1.amazonaws.com","dlpyuedphkuxkv
+","28bf6138b9f274a22697fb6f2d531c02f86b95e700ddb2e1bca20935d7ba57c8","dcok9uvp4rl5h8
+");
     
     
     if ($mysqli -> connect_errno) {
