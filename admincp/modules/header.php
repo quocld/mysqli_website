52324decf35b2b275e560b53835c39aa57e7c1ea
quocle2208 <80745853+quocle2208@users.noreply.github.com>
2021-06-08 16:03:09 +0700

    Update header.php

--- admincp/modules/header.php
+++ admincp/modules/header.php
@@ -1,10 +1,1 @@
-<?php
-	if(isset($_GET['dangxuat'])&&$_GET['dangxuat']==1){
-		unset($_SESSION['dangnhap']);
-		header('Location:login.php');
-	}
-?>
-<p><a href="index.php?dangxuat=1">Đăng xuất : <?php if(isset($_SESSION['dangnhap'])){
-		echo $_SESSION['dangnhap'];
-
-	} ?></a></p>
+<p>header</p>
