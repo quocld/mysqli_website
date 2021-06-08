3bb680159de8e01ac34c41982c4de5dac062394e
quocle2208 <80745853+quocle2208@users.noreply.github.com>
2021-06-08 16:17:16 +0700

    a
    a

---
+++ admincp/modules/managerproduct/editproduct.php
@@ -0,0 +1,30 @@
+<?php
+    $sql_editproduct="SELECT*FROM tbl_category WHERE id_category='$_GET[idproduct]' LIMIT 1 ";
+    $query_editproduct= mysqli_query($mysqli,$sql_editproduct);
+?>
+
+<h1>EDIT PRODUCT</h1>
+<table border="1" width="80%" style="border-collapse: collapse">
+    <form method="POST" action="modules/managerproduct/handling.php?idproduct=<?php echo $_GET['idproduct'] ?>">
+        
+        <?php
+            while($dong= mysqli_fetch_array($query_editproduct)){
+        ?>
+        <tr>
+            <td>Category name</td>
+            <td><input type="text" value="<?php echo $dong['danhmuc']?>" name="tendanhmuc" ></td>
+        </tr>
+        <tr>
+            <td>Category Code</td>
+            <td><input type="text" value="<?php echo $dong['madanhmuc'] ?>" name="madanhmuc" ></td>
+        </tr>
+        <tr>
+            
+            <td colspan="2"><input type="submit" name="suadanhmuc" value="Save" ></td>
+        </tr>
+        <?php 
+            }
+        ?>
+    </form>
+    
+</table>
